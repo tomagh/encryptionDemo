f661b8660bec651d0eb667189ecc496f36823748
Andrew Tomenko <tomabox@me.com>
2017-03-06 09:20:54 +0200

    Initial Commit

---
+++ test.2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test.2
+//
+//  Created by Andrew Tomenko on 3/6/17.
+//  Copyright © 2017 Andrew Tomenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ test.2/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  test.2
+//
+//  Created by Andrew Tomenko on 3/6/17.
+//  Copyright © 2017 Andrew Tomenko. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
